0701e06c11328e4761c32d9cf71f80f1f7b20e2f
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-09-05 14:05:11 +0300

    feat: Edited model test for question_spec.rb to belong_to :author

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   it { should have_many(:answers).dependent(:destroy) }
+  it { should belong_to(:author).class_name('User') }
+
 
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
