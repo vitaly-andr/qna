a2b2f1b4f32e53a3823541db99e7e284108926f4
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-09-06 08:00:48 +0300

    feat: added test author_of

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,6 +5,4 @@ RSpec.describe Answer, type: :model do
   it { should belong_to(:author).class_name('User') }
 
   it { should validate_presence_of(:body) }
-  it { should validate_presence_of(:question) }
-  it { should validate_presence_of(:author) }
 end
