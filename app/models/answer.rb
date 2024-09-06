373052b936cbeacf75f43fd5bea05f5442a213f2
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-09-06 07:41:29 +0300

    refactor: removed unnecessary validations from model answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,7 +3,5 @@ class Answer < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
   validates :body, presence: true
-  validates :question, presence: true
-  validates :author, presence: true
 
 end
