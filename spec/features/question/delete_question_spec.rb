bf5934732b1b7a1b58ae65264d140578fedb54d8
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-09-08 00:08:56 +0300

    feat: Added tests for deleting questions from index

--- spec/features/question/delete_question_spec.rb
+++ spec/features/question/delete_question_spec.rb
@@ -10,7 +10,7 @@ feature 'Author can delete their question', %q(
   given!(:question) { create(:question, author: user) }
   given!(:other_question) { create(:question, author: other_user) }
 
-  scenario 'Author tries to delete their question' do
+  scenario 'Author tries to delete their question from the question page' do
     sign_in(user)
     visit question_path(question)
 
@@ -23,9 +23,31 @@ feature 'Author can delete their question', %q(
   end
 
   scenario 'Non-author tries to delete someone else’s question' do
-    sign_in(user)
-    visit question_path(other_question)
+    sign_in(other_user)
+    visit question_path(question)
 
     expect(page).to_not have_link 'Delete Question'
+  end
+
+  scenario 'Author tries to delete their question from the index page' do
+    sign_in(user)
+    visit questions_path
+
+    within "#question_#{question.id}" do
+      expect(page).to have_link 'Delete Question'
+      click_on 'Delete Question'
+    end
+
+    expect(page).to have_content 'Your question was successfully deleted.'
+    expect(page).to_not have_content question.title_was
+  end
+
+  scenario 'Non-author tries to delete someone else’s question from the index page' do
+    sign_in(other_user)
+    visit questions_path
+    save_and_open_page
+    within "#question_#{question.id}" do
+      expect(page).to_not have_link 'Delete Question'
+    end
   end
 end
