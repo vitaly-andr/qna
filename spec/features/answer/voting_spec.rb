6dd4df3d4fdfa4b9c9a8221123b04c439334136c
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-09-25 15:39:09 +0300

    Test: Voting for a answer

---
+++ spec/features/answer/voting_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+feature 'Voting for an answer', %q{
+  In order to vote for an answer I like
+  As an authenticated user
+  I'd like to be able to vote for or against an answer on the question page
+} do
+  given(:user) { create(:user) }
+  given(:author) { create(:user) }
+  given!(:question) { create(:question, author: author) }
+  given!(:answer) { create(:answer, question: question, author: author) }
+
+  background do
+    sign_in(user)
+    visit question_path(question)
+  end
+
+  scenario 'Authenticated user votes for an answer', js: true do
+    within "##{dom_id(answer)}" do
+      click_on 'Upvote'
+
+      expect(page).to have_content 'Rating: 1'
+    end
+  end
+
+  scenario 'Authenticated user votes against an answer', js: true do
+    within "##{dom_id(answer)}" do
+      click_on 'Downvote'
+
+      expect(page).to have_content 'Rating: -1'
+    end
+  end
+
+  scenario 'Authenticated user cannot vote for their own answer', js: true do
+    sign_out(user)
+    sign_in(author)
+    visit question_path(question)
+
+    within "##{dom_id(answer)}" do
+      expect(page).not_to have_link 'Upvote'
+      expect(page).not_to have_link 'Downvote'
+    end
+  end
+
+  scenario 'Authenticated user can cancel their vote and vote again for an answer', js: true do
+    within "##{dom_id(answer)}" do
+      click_on 'Upvote'
+      expect(page).to have_content 'Rating: 1'
+
+      click_on 'Cancel vote'
+      expect(page).to have_content 'Rating: 0'
+
+      click_on 'Downvote'
+      expect(page).to have_content 'Rating: -1'
+    end
+  end
+
+  scenario 'Authenticated user can vote only once per answer', js: true do
+    within "##{dom_id(answer)}" do
+      click_on 'Upvote'
+      expect(page).to have_content 'Rating: 1'
+
+      click_on 'Upvote'
+      expect(page).to have_content 'You have already voted'
+    end
+  end
+end
