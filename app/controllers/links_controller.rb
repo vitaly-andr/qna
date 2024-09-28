6ba208c876afe016888644d2fe8f957f95df25f9
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-09-28 18:16:21 +0300

    Refactor: Added statuses to responses in links controller Also corrected render

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -6,13 +6,19 @@ class LinksController < ApplicationController
     if current_user.author_of?(link.linkable)
       link.destroy
       respond_to do |format|
-        format.turbo_stream { render turbo_stream: turbo_stream.remove(view_context.dom_id(link)) }
+        format.turbo_stream {
+          render [ turbo_stream.remove(view_context.dom_id(link)),
+                   render_flash_notice('Link was successfully removed.')
+                 ]
+        }
         format.html { redirect_back fallback_location: root_path, notice: 'Link was successfully removed.' }
       end
     else
       respond_to do |format|
-        format.html { redirect_back fallback_location: root_path, alert: 'You are not authorized to delete this link.' }
-        format.turbo_stream { render_flash_alert('You are not authorized to delete this link.') }
+        format.html { redirect_back fallback_location: root_path, alert: 'You are not authorized to delete this link.', status: :forbidden }
+        format.turbo_stream {
+          render render_flash_alert('You are not authorized to delete this link.'), status: :forbidden
+        }
       end
     end
   end
