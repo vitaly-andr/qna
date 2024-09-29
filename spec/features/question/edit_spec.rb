f62dab9aa60599411718b30a034c62bcda38248f
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-09-29 14:51:26 +0300

    Fix: When uploading via Active Storage by default if you upload new files it deletes all the existing uploads and replaces them with the new ones. Even if did not uploading new files
    Вот правильное решение
    https://stackoverflow.com/a/79036231/15344962
    
    Это мой ответ

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -12,6 +12,7 @@ feature 'Author can edit their question', %q(
   scenario 'Author edits their question from the index page, attaches files, and updates links' do
     create(:link, name: 'Gist link', url: 'https://gist.github.com/vitaly-andr/83bdcd7a1a1282cb17085714494ded2a', linkable: question)
     create(:link, name: 'GitHub', url: 'https://github.com', linkable: question)
+    question.files.attach(io: File.open("#{Rails.root}/spec/fixtures/files/old_image.webp"), filename: 'old_image.webp', content_type: 'image/webp')
 
     sign_in(user)
     visit questions_path
@@ -43,11 +44,46 @@ feature 'Author can edit their question', %q(
       expect(page).to have_content 'Edited Question Title'
       expect(page).to have_content 'Edited Question Body'
 
+      expect(page).to have_link 'old_image.webp'
+
       expect(page).to have_link 'rails_helper.rb'
       expect(page).to have_link 'spec_helper.rb'
 
       expect(page).to have_link 'Updated Gist Link', href: 'https://gist.github.com/vitaly-andr/83bdcd7a1a1282cb17085714494ded2a'
       expect(find('div[data-url="https://google.com"]')['data-url']).to eq 'https://google.com'
+    end
+  end
+  scenario 'Author attaches files to the question via file input click' do
+    # Используем заранее прикрепленный файл reward_image.webp как старый файл
+    question.files.attach(io: File.open("#{Rails.root}/spec/fixtures/files/old_image.webp"), filename: 'old_image.webp', content_type: 'image/webp')
+
+    sign_in(user)
+    visit questions_path
+
+    within "turbo-frame##{dom_id(question)}" do
+      click_on 'Edit Question'
+
+      fill_in 'Title', with: 'Edited Question Title'
+      fill_in 'Body', with: 'Edited Question Body'
+
+      # Клик по полю для загрузки файлов
+      # find('label[for="question_files"]').click
+
+      # Используем attach_file для выбора файлов после клика
+      attach_file 'question_files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"], visible: false
+
+      click_on 'Save'
+
+      # Проверяем, что файлы успешно прикреплены и отображены
+      expect(page).to have_no_selector 'textarea'
+      expect(page).to have_content 'Edited Question Title'
+      expect(page).to have_content 'Edited Question Body'
+      # Проверка наличия старого файла
+      expect(page).to have_link 'old_image.webp'
+
+      # Проверка наличия новых файлов
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
     end
   end
 
@@ -99,7 +135,6 @@ feature 'Author can edit their question', %q(
 
       expect(page).to have_link 'rails_helper.rb'
       expect(page).to have_link 'spec_helper.rb'
-      save_and_open_page
       expect(page).to have_link 'Updated Gist Link', href: 'https://gist.github.com/vitaly-andr/83bdcd7a1a1282cb17085714494ded2a'
       expect(find('div[data-url="https://google.com"]')['data-url']).to eq 'https://google.com'
     end
