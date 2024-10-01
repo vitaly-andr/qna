d3de821fc7d85ed340350d5f44da20f256b1fa61
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-10-01 20:25:58 +0300

    Feature: Real time comments in Live Feed

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -12,16 +12,22 @@ class Answer < ApplicationRecord
   validates :body, presence: true
 
   after_create_commit do
-    broadcast_prepend_later_to "questions", target: "question_#{question.id}_answers", partial: "live_feed/answer", locals: { answer: self }
-    broadcast_remove_to "questions", target: "no-answers-#{question.id}"
+    broadcast_prepend_to "questions", target: "question_#{question.id}_answers", partial: "live_feed/answer", locals: { answer: self }
   end
 
   after_update_commit do
-    broadcast_replace_later_to "questions", target: "answer_#{id}", partial: "live_feed/answer", locals: { answer: self }
+    broadcast_update_to "questions",
+                        target: "#{dom_id(self)}",
+                        partial: "live_feed/answer",
+                        locals: { answer: self }
   end
 
   after_destroy_commit do
     broadcast_remove_to "questions", target: "answer_#{id}"
   end
+  private
 
+  def dom_id(record, prefix = nil)
+    ActionView::RecordIdentifier.dom_id(record, prefix)
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,21 @@ class Comment < ApplicationRecord
 
   belongs_to :commentable, polymorphic: true
   validates :body, presence: true
+  after_create_commit do
+    broadcast_prepend_later_to "questions",
+                               target: "#{dom_id(commentable, :comments)}",
+                               partial: 'comments/comment',
+                               locals: { comment: self }
 
+    broadcast_remove_to "questions", target: "#{dom_id(commentable, :no_comments)}"
+    broadcast_update_to "questions",
+                        target: "#{dom_id(commentable, :form)}",
+                        partial: 'comments/comment_button',
+                        locals: { commentable: commentable }
+  end
+  private
+
+  def dom_id(record, prefix = nil)
+    ActionView::RecordIdentifier.dom_id(record, prefix)
+  end
 end
