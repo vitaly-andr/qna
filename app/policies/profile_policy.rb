c0b6766a58c61b98120d165e35c0fbddeacfc626
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-10-10 20:41:14 +0300

    Tests: API profiles

--- app/policies/profile_policy.rb
+++ app/policies/profile_policy.rb
@@ -2,4 +2,14 @@ class ProfilePolicy < ApplicationPolicy
   def me?
     true
   end
+
+  def index?
+    user.present?
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
 end
