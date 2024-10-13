9e8c9d894c923cb79c4c70a027d34956ed97aad9
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-10-13 15:50:44 +0300

    refactor: notification_service.rb subscribers request

--- app/services/notification_service.rb
+++ app/services/notification_service.rb
@@ -9,7 +9,7 @@ class NotificationService
   end
 
   def call
-    subscribers = @question.subscribers - [@answer.author]
+    subscribers = @question.users.where.not(id: @answer.author_id)
     subscribers.each do |subscriber|
       SubscriptionMailer.new_answer_notification(@answer, subscriber).deliver_later
       Rails.logger.info "Sending new answer notification async to #{subscriber.email} for question #{@question.id}"
