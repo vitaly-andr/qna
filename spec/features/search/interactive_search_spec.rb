6a80254ebf64f1cf623a4d398e2503ec00d3f0fd
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-10-14 17:29:30 +0300

    Fix views: feature tests for Elastic Search interactive

--- spec/features/search/interactive_search_spec.rb
+++ spec/features/search/interactive_search_spec.rb
@@ -12,13 +12,22 @@ feature 'User can search dynamically', %q{
   given!(:user) { create(:user) }
 
   background do
+    Question.reindex
+    Answer.reindex
+    Comment.reindex
+    User.reindex
+
+    Question.search_index.refresh
+    Answer.search_index.refresh
+    Comment.search_index.refresh
+    User.search_index.refresh
+
     visit root_path
     click_link 'Search', target: '_top' # Clicking the search link to navigate to the search page
   end
 
   scenario 'User sees results as they type a valid query', js: true do
     fill_in 'Search...', with: 'searchkick'
-    save_and_open_page
     # Check that search results are displayed on the screen
     expect(page).to have_content 'How to integrate searchkick in Rails?'
     expect(page).to have_content 'Searchkick is a full-text search engine'
@@ -32,13 +41,14 @@ feature 'User can search dynamically', %q{
     expect(page).to have_content 'How to integrate searchkick in Rails?'
 
     # Remove text to less than 3 characters
-    fill_in 'Search...', with: 'se'
 
+    fill_in 'Search...', with: 'se'
     # Check that the "No results found" message is displayed
     expect(page).to have_content 'No results found'
   end
 
   scenario 'User sees "No results found" for invalid query', js: true do
+
     fill_in 'Search...', with: 'Non-existing content'
 
     # Check that the "No results found" message is displayed
