66457fea3617e5c8d7fe86e4dfc427d79111ad47
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-10-17 08:47:40 +0300

    Installed capistrano

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,8 +1,20 @@
-Searchkick.client = Elasticsearch::Client.new(
-  url: ENV['ELASTICSEARCH_URL'] || 'https://localhost:9200',
-  transport_options: {
-    ssl: { verify: false }
-  },
-  user: 'elastic',
-  password: ENV['ELASTICSEARCH_PASSWORD']
-)
+if Rails.env.production?
+  Searchkick.client = Elasticsearch::Client.new(
+    url: Rails.application.credentials.dig(:elasticsearch, :host),
+    transport_options: {
+      ssl: { verify: false } # Временно отключаем проверку сертификата
+    },
+    user: Rails.application.credentials.dig(:elasticsearch, :username),
+    password: Rails.application.credentials.dig(:elasticsearch, :password)
+  )
+else
+  # Development and test configuration
+  Searchkick.client = Elasticsearch::Client.new(
+    url: ENV['ELASTICSEARCH_URL'] || 'https://localhost:9200',
+    transport_options: {
+      ssl: { verify: false }
+    },
+    user: 'elastic',
+    password: ENV['ELASTICSEARCH_PASSWORD']
+  )
+end
