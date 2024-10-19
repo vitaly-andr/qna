ec3ef8af08059a698add87ef284c0755210e6d21
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-10-19 21:20:59 +0300

    ...

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,20 +1,14 @@
 if Rails.env.production?
+  Rails.logger.debug "Elasticsearch host: #{Rails.application.credentials.dig(:elasticsearch, :host)}"
+  Rails.logger.debug "Elasticsearch username: #{Rails.application.credentials.dig(:elasticsearch, :username)}"
+  Rails.logger.debug "Elasticsearch password: #{Rails.application.credentials.dig(:elasticsearch, :password)}"
+
   Searchkick.client = Elasticsearch::Client.new(
     url: Rails.application.credentials.dig(:elasticsearch, :host),
     transport_options: {
-      ssl: { verify: false } # Временно отключаем проверку сертификата
+      ssl: { verify: false }
     },
     user: Rails.application.credentials.dig(:elasticsearch, :username),
     password: Rails.application.credentials.dig(:elasticsearch, :password)
-  )
-else
-  # Development and test configuration
-  Searchkick.client = Elasticsearch::Client.new(
-    url: ENV['ELASTICSEARCH_URL'] || 'https://localhost:9200',
-    transport_options: {
-      ssl: { verify: false }
-    },
-    user: 'elastic',
-    password: ENV['ELASTICSEARCH_PASSWORD']
   )
 end
